70d5fae0a0af7e2cc48886a8b249c75c4cf21d27
yijie4188 <yijie4188@gmail.com>
2021-04-25 22:32:31 +0800

    chore(test): Add property test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,32 +4,41 @@ import cap from 'chai-as-promised'
 
 use(cap)
 
-let output = ''
+describe('simple', async () => {
+  let output = ''
 
-class A {
-  /** sync method */
-  foo() {
-    output += '1'
-    return this
+  class A {
+    /** sync method */
+    foo() {
+      output += '1'
+      return this
+    }
+
+    /** async method */
+    async bar() {
+      output += '2'
+      return this
+    }
+
+    /** property */
+    baz = 3
+
+    /** getter */
+    get qux() {
+      return this.baz
+    }
   }
 
-  /** async method */
-  async bar() {
-    output += '2'
-    return this
-  }
+  it('basic support', async () => {
+    const a = wrap(new A())
+    await expect(a.foo().bar().foo().bar().bar().qux).to.eventually.equal(3)
+    expect(output).to.equal('12122')
+  })
 
-  /** property */
-  baz = 3
-
-  /** getter */
-  get qux() {
-    return this.baz
-  }
-}
-
-it('basic support', async () => {
-  const a = wrap(new A())
-  await expect(a.foo().bar().foo().bar().bar().qux).to.eventually.equal(3)
-  expect(output).to.equal('12122')
+  it('property support', async () => {
+    const originA = new A()
+    const a = wrap(originA)
+    a.baz = 1
+    expect(originA.baz).to.equal(1)
+  })
 })
