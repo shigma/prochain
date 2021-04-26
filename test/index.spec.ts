2d0977852829fb226684bdaf9d0647bf9708ba5d
yijie4188 <yijie4188@gmail.com>
2021-04-26 22:42:57 +0800

    chore(test): Supplement decorator unit test.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,44 +1,62 @@
 import { expect, use } from 'chai'
-import { wrap } from 'prochain'
+import { Wrap, wrap, wrapDec } from 'prochain'
 import cap from 'chai-as-promised'
 
 use(cap)
 
-describe('simple', async () => {
-  let output = ''
-
-  class A {
-    /** sync method */
-    foo() {
-      output += '1'
-      return this
-    }
-
-    /** async method */
-    async bar() {
-      output += '2'
-      return this
-    }
-
-    /** property */
-    baz = 3
-
-    /** getter */
-    get qux() {
-      return this.baz
-    }
+let output = ''
+class A {
+  /** sync method */
+  foo() {
+    output += '1'
+    return this
   }
 
+  /** async method */
+  async bar() {
+    output += '2'
+    return this
+  }
+
+  /** property */
+  baz = 3
+
+  /** getter */
+  get qux() {
+    return this.baz
+  }
+}
+
+describe('simple', async () => {
   it('basic support', async () => {
     const a = wrap(new A())
     await expect(a.foo().bar().foo().bar().bar().qux).to.eventually.equal(3)
     expect(output).to.equal('12122')
   })
+})
 
-  it('property support', async () => {
-    const originA = new A()
-    const a = wrap(originA)
-    a.baz = 1
-    expect(originA.baz).to.equal(1)
+describe('decorator', async () => {
+  @wrapDec
+  class B extends A {
+  }
+  interface B { stream: Wrap<B> }
+
+  it('basic support', async () => {
+    const b = new B()
+    await expect(b.stream.foo().bar().foo().bar().bar().qux)
+      .to.eventually.equal(3)
+    expect(output).to.equal('12122')
+  })
+
+  @wrapDec('stream1')
+  class C extends A {
+  }
+  interface C { stream1: Wrap<C> }
+
+  it('attr param support', async () => {
+    const c = new C()
+    await expect(c.stream1.foo().bar().foo().bar().bar().qux)
+      .to.eventually.equal(3)
+    expect(output).to.equal('12122')
   })
 })
